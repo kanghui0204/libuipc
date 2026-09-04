737811fe291251d8ffc975d06a00105ba136755b
OrangeEarth15 <1523158798@qq.com>
2026-09-04 07:01:25 -0700

    perf(cuda): reduce four-vertex contact PSD to 9D

--- src/backends/cuda/contact_system/contact_models/ipc_simplex_normal_contact.cu
+++ src/backends/cuda/contact_system/contact_models/ipc_simplex_normal_contact.cu
@@ -3,6 +3,7 @@
 #include <utils/distance/distance_flagged.h>
 #include <utils/codim_thickness.h>
 #include <utils/fixed_bank_soa_evd.h>
+#include <utils/four_vertex_translation_free_spd.h>
 #include <kernel_cout.h>
 #include <utils/matrix_assembler.h>
 #include <utils/primitive_d_hat.h>
@@ -321,7 +322,8 @@ namespace
                 FixedBankSoAMap<12, SharedLanePitch> H(shared_h + threadIdx.x);
                 Vector12 eigen_values;
                 PT_barrier_gradient_hessian(G, H, flag, kt2, d_hat, thickness, P, T0, T1, T2);
-                selfadjoint_evd_fixed_bank_shared<12>(H, eigen_values);
+                selfadjoint_evd_four_vertex_translation_free_fixed_bank<
+                    SharedLanePitch>(H, eigen_values);
                 DoubletVectorAssembler DVA{PT_Gs};
                 DVA.segment<4>(i * 4).write(PT, G);
                 TripletMatrixAssembler TMA{PT_Hs};
@@ -370,7 +372,8 @@ namespace
                 Vector12 eigen_values;
                 mollified_EE_barrier_gradient_hessian(
                     G, H, flag, kt2, d_hat, thickness, t0_Ea0, t0_Ea1, t0_Eb0, t0_Eb1, E0, E1, E2, E3);
-                selfadjoint_evd_fixed_bank_shared<12>(H, eigen_values);
+                selfadjoint_evd_four_vertex_translation_free_fixed_bank<
+                    SharedLanePitch>(H, eigen_values);
                 DoubletVectorAssembler DVA{EE_Gs};
                 DVA.segment<4>(i * 4).write(EE, G);
                 TripletMatrixAssembler TMA{EE_Hs};
